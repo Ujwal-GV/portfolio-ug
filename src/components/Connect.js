74e614c4f3969b895b776ec94f36439bdb9a261f
unknown <ujwalgowda2002v@gmail.com>
2024-06-03 17:33:12 +0530

    app.css changes

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -7,9 +7,9 @@ export default function Connect() {
   return (
     <div className='nav-footer' id='connect'>
       <div className="box" style={{color: "white"}}>CONNECT WITH ME:</div>
-      <div className="box"><a href="mailto:ujwalgowda2002v@gmail.com"><IoMdMail style={{color: "white", width: "30px", height: "30px"}} /><br />E-mail</a></div>
-      <div className="box"><a href="tel:+91 7483262684"><FaPhone style={{color: "white", width: "20px", height: "20px"}} /><br />Phone</a></div>
-      <div className="box"><a href="https://www.linkedin.com/in/ujwal-gowda-v-5ba277276" rel="noreferrer" target='_blank'><FaLinkedin style={{color: "white", width: "30px", height: "30px"}} /><br />Linkedin</a></div>
+      <div className="box"><a href="mailto:ujwalgowda2002v@gmail.com"><IoMdMail style={{color: "white", width: "30px", height: "30px", marginRight: "5px"}} />E-mail</a></div>
+      <div className="box"><a href="tel:+91 7483262684"><FaPhone style={{color: "white", width: "20px", height: "20px", marginRight: "5px"}} />Phone</a></div>
+      <div className="box"><a href="https://www.linkedin.com/in/ujwal-gowda-v-5ba277276" rel="noreferrer" target='_blank'><FaLinkedin style={{color: "white", width: "30px", height: "30px", marginRight: "5px"}} />Linkedin</a></div>
     </div>
   )
 }
